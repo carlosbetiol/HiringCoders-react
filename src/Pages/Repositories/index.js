656da31cde34bbcd3e8762b24c9758e247f9d8b4
Carlos Betiol <carlos@sistelpa.com>
2021-07-16 11:38:59 -0400

    incluido exemplos das aulas 8 e 9

--- src/Pages/Repositories/index.js
+++ src/Pages/Repositories/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styled';
 
 export default function Repositories() {
+    const [ repositories, setRepositories ] = useState([]);
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        repositoriesName = JSON.parse(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear();
+    }, []); // parametro vazio pq eh para fazer quando ja entra na pagina, sem variavel monitorada
+
     return (
         <S.Container>
         <S.Title>Repositórios</S.Title>
         <S.List>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+            { repositories.map(repository => {
+                return (
+                    <S.ListItem>Repositório: { repository }</S.ListItem>
+                )
+            })}
+            
         </S.List>
         </S.Container>
     )
